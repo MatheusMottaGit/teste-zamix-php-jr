fd13f03bfc8edf4b445c887aaabf3597b15f68a6
Matheus Motta <matheusdomingues423@gmail.com>
2025-08-05 02:27:33 -0300

    sql scripts for reports (in/out)

---
+++ sql/in_report.sql
@@ -0,0 +1,5 @@
+select sm.quantity, products.name,(sm.cost_price * sm.quantity) as total_cost_price, (products.sale_price * sm.quantity) as total_sale_price
+from stock_movements sm
+join products on products.id = sm.product_id
+where sm.type = 'in' 
+and sm.movement_date between '2025-08-01' and '2025-08-31';
